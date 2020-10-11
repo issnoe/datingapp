b3bbff6cfe495a953b166d481b5e2631732553f5
issnoe <issnoe@issnoe.local>
2020-10-11 12:51:30 -0500

    UI, Add dummy components

---
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-details',
+  templateUrl: './member-detail.component.html',
+  styleUrls: ['./member-detail.component.css']
+})
+export class MemberDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
