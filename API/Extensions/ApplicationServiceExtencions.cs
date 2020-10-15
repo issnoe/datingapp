3a874c06ae6dfac13be37666e360e656e9f665e3
issnoe <issnoe@issnoe.local>
2020-10-15 09:10:06 -0500

    API, Entitty - Factory Pattern Config

--- API/Extensions/ApplicationServiceExtencions.cs
+++ API/Extensions/ApplicationServiceExtencions.cs
@@ -4,13 +4,24 @@ using API.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using API.Helpers;
 
 namespace API.Extensions
 {
     public static class ApplicationServiceExtencions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config){
-             services.AddScoped<ITokenService, TokenServices>();
+            // Auth midleware
+            services.AddScoped<ITokenService, TokenServices>();
+
+            // Repository Pattern 
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            // Auto Mapper Helps to Cast the Entity on a new Interface or works as a  DTO implementation
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+
+            // Coonect database
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
